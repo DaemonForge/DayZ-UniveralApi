d5ae698bed99d92becbed8a316eea3f71ffb1715
Kevin Hoddinott <kevhoddinott@gmail.com>
2020-12-18 15:32:07 -0500

    Adding a constructor to allow for single line calls for queries

--- _UniversalApi/scripts/3_Game/Objects/QueryObject.c
+++ _UniversalApi/scripts/3_Game/Objects/QueryObject.c
@@ -9,9 +9,20 @@ class UApiQueryObject{
 	
 	string OrderBy = "{}"; //The OrderBy for the Query https://docs.mongodb.com/manual/reference/operator/meta/orderby/
 	
-	string ReturnObject = ""; //
+	string ReturnObject = ""; //This if you want to return a specific value within the document
 	
-	int MaxResults = -1;
+	int MaxResults = -1;  //Max Number of Results to return, Note: dayz seems to crash at over 30mb in some of my tests
+	
+	
+	
+	void UApiQueryObject(string query = "{}", string orderBy = "{}", int maxResults = -1, string returnObject = ""){
+		
+		Query = query;
+		OrderBy = orderBy;
+		ReturnObject = returnObject;
+		MaxResults = maxResults;
+		
+	}
 	
 	string ToJson(){
 		return JsonFileLoader<UApiQueryObject>.JsonMakeData(this);;
