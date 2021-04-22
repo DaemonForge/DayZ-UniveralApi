d2025e41014d8a729eafe6773a0d237d578962ff
Daemon Forge <kevhoddinott@gmail.com>
2021-04-22 19:06:07 -0400

    Update Utils.c

--- _UniversalApi/scripts/3_Game/Utils.c
+++ _UniversalApi/scripts/3_Game/Utils.c
@@ -1,5 +1,7 @@
 static const int UAPI_SUCCESS = 200;
+static const int UAPI_EMPTY = 204;
 static const int UAPI_TIMEOUT = 408;
+static const int UAPI_CLIENTERROR = 400;
 static const int UAPI_SERVERERROR = 500;
 static const int UAPI_ERROR = 418;
 static const int UAPI_NOTFOUND = 404;
@@ -8,6 +10,7 @@ static const int UAPI_UNAUTHORIZED = 401;
 
 
 static const int UAPI_DBSUCCESS = 200;
+static const int UAPI_DBEMPTY = 204;
 static const int UAPI_DBTIMEOUT = 408;
 static const int UAPI_DBSERVERERROR = 500;
 
