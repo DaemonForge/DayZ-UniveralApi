0763d8c9bd40040784d870c0427c583d4a79ca28
Daemon Forge <kevhoddinott@gmail.com>
2021-07-05 14:28:09 -0400

    Move QueryResult Template to own file

--- _UniversalApi/scripts/3_Game/ApiQuery.c
+++ _UniversalApi/scripts/3_Game/ApiQuery.c
@@ -29,29 +29,3 @@ class ApiQueryBase : RestCallback{
 	};
 	
 }
-
-
-class UApiQueryResult<Class T> : StatusObject {
-	
-	autoptr array<ref T> Results;
-	int Count;
-	
-	
-	static UApiQueryResult<T> CreateFrom(string  stringData){
-		UApiQueryResult<T> returnval;
-		if (UApiJSONHandler<UApiQueryResult<T>>.FromString( stringData, returnval)){
-			return returnval;
-		} 
-		Error("[UAPI] Failed to create Query Results");
-		return NULL;
-	}
-	
-	bool FromJson(string stringData) {
-		return UApiJSONHandler<UApiQueryResult<T>>.FromString( stringData, this);
-	}
-
-	
-	array<ref T> GetResults(){
-		return Results;
-	}
-}
