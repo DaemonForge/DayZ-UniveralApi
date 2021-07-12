6d275dbe7c1997fa8a688dd310b063636fad05d6
Daemon Forge <kevhoddinott@gmail.com>
2021-07-11 21:56:21 -0400

    Adding Status Response Object and Version Check Function

--- _UniversalApi/scripts/3_Game/Objects/StatusObject.c
+++ _UniversalApi/scripts/3_Game/Objects/StatusObject.c
@@ -35,3 +35,46 @@ class UApiRandomNumberResponse extends StatusObject {
 	autoptr TIntArray Numbers;
 	
 }
+
+class UApiStatus extends StatusObject {
+	
+    string Version =  "0.0.0";
+	string Discord =  "Disabled";
+	string Translate =  "Disabled";
+	autoptr TStringArray Wit;
+	autoptr TStringArray QnA;
+	autoptr TStringArray LUIS;
+	
+	
+	int CheckVersion(string version){
+		if (version == Version){
+			return 0;
+		}
+		TStringArray ModVerMap = {};
+		TStringArray ApiVerMap = {};
+		version.Split(".", ModVerMap);
+		Version.Split(".", ApiVerMap);
+		int ModMajor = ModVerMap.Get(0).ToInt();
+		int ModMinor = ModVerMap.Get(1).ToInt();
+		int ModPatch = ModVerMap.Get(2).ToInt();
+		int ApiMajor = ApiVerMap.Get(0).ToInt();
+		int ApiMinor = ApiVerMap.Get(1).ToInt();
+		int ApiPatch = ApiVerMap.Get(2).ToInt();
+		if (ModMajor > ApiMajor){
+			return 3;
+		} else if (ModMinor > ApiMinor){
+			return 2;
+		} else if (ModPatch > ApiPatch){
+			return 1;
+		}
+		if (ModMajor < ApiMajor){
+			return -3;
+		} else if (ModMinor < ApiMinor){
+			return -2;
+		} else if (ModPatch < ApiPatch){
+			return -1;
+		}
+		return 0;
+	}
+	
+}
