5109c546007924ade233e6cf1d9840226b2f9d74
Daemon Forge <kevhoddinott@gmail.com>
2021-07-23 22:22:50 -0400

    Remove Random Test Script left in by accident

--- _UniversalApi/scripts/5_Mission/MissionBase.c
+++ _UniversalApi/scripts/5_Mission/MissionBase.c
@@ -13,22 +13,5 @@ modded class MissionBase extends MissionBaseWorld
 		//Don't forget to Super As
 		//super.UniversalApiReady();
 		
-		GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(this.TestRandom, 10000, true);
-	}
-	
-	void TestRandom(){
-		
-		Print("Random: " + Math.QRandomInt());
-		Print("QRandomInt: 0-10 " + Math.QRandomInt(0,10));
-		Print("QRandomInt: 0-100 " + Math.QRandomInt(0,100));
-		Print("QRandomInt: 0-5 " + Math.QRandomInt(0,5));
-		Print("QRandomFlip: " + Math.QRandomFlip());
-		Print("QRandomFlip: " + Math.QRandomFlip());
-		Print("QRandomFlip: " + Math.QRandomFlip());
-		Print("QRandomFloat: 0-1 " + Math.QRandomFloat(0,1));
-		Print("QRandomFloat: 0-1 " + Math.QRandomFloat(0,1));
-		Print("QRandomFloat: 0-10 " + Math.QRandomFloat(0,10));
-		Print("QRandomFloat: 5-10 " + Math.QRandomFloat(5,10));
-		Print("QRandomFloat: 10000-100000 " + Math.QRandomFloat(10000,100000));
 	}
 }
