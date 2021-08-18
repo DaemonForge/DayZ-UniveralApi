8f5cc5e21ad51834a5268d6c10b6c4f11425ac7a
Daemon Forge <kevhoddinott@gmail.com>
2021-08-17 21:05:12 -0400

    Fixing Compiler error when using DB Handler

--- _UniversalApi/scripts/3_Game/UApi/Endpoints/UApiDBHandler.c
+++ _UniversalApi/scripts/3_Game/UApi/Endpoints/UApiDBHandler.c
@@ -1,23 +1,31 @@
-/* Just an idea?? for future implmentations 
+/* 
+	Template DB Handler
+	This is the newest Method in which modders can interact with the Universal API's MongoDB Endpoints providing access to modders to be
+	able to build mods that utilize the Universal API Webservice.
+	
+	view full documentation https://github.com/daemonforge/DayZ-UniveralApi/wiki/Developer-Reference
 
 	static autoptr UApiDBHandler<myClass> m_MyModHandler = new UApiDBHandler<myClass>("MyMod", PLAYER_DB); //PLAYER_DB or OBJECT_DB
 	
 	m_MyModHandler.Save("GUID", myObject); //returns Call ID
 	m_MyModHandler.Load("GUID", player, "MyCallBackFunction"); //returns Call ID
+
+
 	
 */
 
-
 class UApiDBHandler<Class T> extends UApiDBHandlerBase{
 
-	//Load and Save
 	/*
+	Load and Save
+	
 	CALLBACK FUNCTION EXAMPLE
 	protected void MyCallBackFunction(int cid, int status, string guid, myClass data) {
 		if ( status == UAPI_SUCCESS ){
 			//Do something with data
 		}
 	}*/
+	
 	
 	override int Save(string oid, Class object) {
 		string jsonString = "{}";
@@ -28,7 +36,6 @@ class UApiDBHandler<Class T> extends UApiDBHandlerBase{
 		Error2("[UAPI] DB HANDLER Save", "Error convertering to JSON or casting make sure you are passing the right class type");
 		return -1;
 	}
-	
 	override int Save(string oid, Class object, Class cbInstance, string cbFunction) {
 		string jsonString = "{}";
 		T obj; //Might not need Casting here but using it anyways
@@ -39,10 +46,15 @@ class UApiDBHandler<Class T> extends UApiDBHandlerBase{
 		return -1;
 	}
 	
-	override int Load(string oid, Class cbInstance, string cbFunction, string defaultJson = "{}") {
+	
+	
+	
+	override int Load(string oid, Class cbInstance, string cbFunction) {
+		return UApi().db(Database).Load(Mod,oid, new UApiCallback<T>(cbInstance, cbFunction), "{}");
+	}
+	override int Load(string oid, Class cbInstance, string cbFunction, string defaultJson) {
 		return UApi().db(Database).Load(Mod,oid, new UApiCallback<T>(cbInstance, cbFunction), defaultJson);
 	}
-	
 	override int Load(string oid, Class cbInstance, string cbFunction, Class inObject) {
 		string jsonString = "{}";
 		T obj; //Might not need Casting here but using it anyways
@@ -56,8 +68,13 @@ class UApiDBHandler<Class T> extends UApiDBHandlerBase{
 	}
 	
 	
-	//Query Will return a UApiQueryResult with your Class Callback function for this would be
+	
 	/*
+	
+	Query
+	
+	
+	Query Will return a UApiQueryResult with your Class Callback function for this would be
 	
 	//CALLBACK FUNCTION EXAMPLE
 	protected void MyCallBackFunction(int cid, int status, string guid, UApiQueryResult<myClass> data) {
@@ -74,6 +91,7 @@ class UApiDBHandler<Class T> extends UApiDBHandlerBase{
 	Query Example "{ \"MyTStrringArray\": "somevalue" }" would return all objects that have a value of 'somevalue' inside the array
 	Query Example "{ \"MySubObject.SubVar\": 3 }" would return all objects that have a SubVar = 3 inside a sub object this also works if the sub object was an array
 	
+	
 	*/
 	override int Query(UApiQueryBase query, Class cbInstance, string cbFunction) {
 		return UApi().db(Database).Query(Mod,query,new UApiCallback<UApiQueryResult<T>>(cbInstance, cbFunction));
@@ -86,7 +104,7 @@ class UApiDBHandler<Class T> extends UApiDBHandlerBase{
 
 
 
-//just to be able to manage them in like an array or map maybe later?
+//just to be able to manage them in like an array or map?
 class UApiDBHandlerBase extends Managed {
 	
 	string Mod = "";
@@ -107,18 +125,26 @@ class UApiDBHandlerBase extends Managed {
 		return -1;
 	}
 	
-	int Load(string oid, Class cbInstance, string cbFunction, string defaultJson = "{}") {
+	int Load(string oid, Class cbInstance, string cbFunction) {
 		Error2("[UAPI] UApiDBHandlerBase LOAD","Incorrect Ussage class is not type of UApiDBHandler<T>");
 		return -1;
 	}
 	
-	int Load(string oid, Class cbInstance, string cbFunction, Class defaultObject) {
+	int Load(string oid, Class cbInstance, string cbFunction, string defaultJson) {
 		Error2("[UAPI] UApiDBHandlerBase LOAD","Incorrect Ussage class is not type of UApiDBHandler<T>");
 		return -1;
 	}
 	
-	//LoadJson Returns string instead of object
+	int Load(string oid, Class cbInstance, string cbFunction, Class inObject){
+		Error2("[UAPI] UApiDBHandlerBase LOAD","Incorrect Ussage class is not type of UApiDBHandler<T>");
+		return -1;
+	}
+	
+	
 	/*
+	
+		LoadJson Returns string instead of object
+	
 		CALLBACK FUNCTION EXAMPLE
 		protected void MyCallBackFunction(int cid, int status, string guid, string data) {
 			if ( status == UAPI_SUCCESS ){
@@ -138,11 +164,13 @@ class UApiDBHandlerBase extends Managed {
 		return Transaction(Mod, element, value);
 	}
 	
-	//Transactions
 	/*
-	Updates a sub value inside the object in the database then returns the new value only works with floats or ints
-	Sub objects can be used with dot notation aka MySubObject.SubObjectVar
-	Will return status of UAPI_SUCCESS if operations was successful
+		Transactions
+	
+	
+		Updates a sub value inside the object in the database then returns the new value only works with floats or ints
+		Sub objects can be used with dot notation aka MySubObject.SubObjectVar
+		Will return status of UAPI_SUCCESS if operations was successful
 	*/
 	int Transaction(string oid, string element, float value) {
 		return UApi().db(Database).Transaction(Mod,oid,element,value);
@@ -155,23 +183,24 @@ class UApiDBHandlerBase extends Managed {
 	}
 	
 	
-	//Update
 	/*
-	Updates a sub value inside the object in the database, can also use other operations
-	https://github.com/daemonforge/DayZ-UniveralApi/blob/master/_UniversalApi/scripts/1_Core/Constants.c#L30
-
-	Values can be in JSON format to update or push elements into arrays
+	Update
 	
-	Sub objects can be used with dot notation aka MySubObject.SubObjectVar
-	will return status of UAPI_SUCCESS if operations was successful
+		Updates a sub value inside the object in the database, can also use other operations
+		https://github.com/daemonforge/DayZ-UniveralApi/blob/master/_UniversalApi/scripts/1_Core/Constants.c#L30
+	
+		Values can be in JSON format to update or push elements into arrays
+		
+		Sub objects can be used with dot notation aka MySubObject.SubObjectVar
+		will return status of UAPI_SUCCESS if operations was successful
 	*/
 	int Update(string oid, string element, string value, string operation = UpdateOpts.SET) {
 		return UApi().db(Database).Update(Mod, oid, element, value, operation);
 	}
-	
 	int Update(string oid, string element, string value, string operation, Class cbInstance, string cbFunction) {	
 		return UApi().db(Database).Update(Mod, oid, element, value, operation, new UApiCallback<UApiUpdateResponse>(cbInstance, cbFunction) );
 	}
+	
 	
 	int Query(UApiQueryBase query, Class cbInstance, string cbFunction) {
 		Error2("[UAPI] UApiDBHandlerBase QUERY","Incorrect Ussage class is not type of UApiDBHandler<T>");
@@ -182,6 +211,12 @@ class UApiDBHandlerBase extends Managed {
 		return -1;
 	}
 	
+	
+	/* 
+		Call Cancel
+		
+		This allows you to cancel a call back to prevent access violations 
+	*/
 	void Cancel(int cid){
 		UApi().RequestCallCancel(cid);
 	}
