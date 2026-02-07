55f7bf8bc1fee1c46a192fbc2134d5a745bc5523
Daemon Forge <kevhoddinott@gmail.com>
2026-02-06 22:39:44 -0500

    Removing Vanilla weapons from Default textures
    Delete paintable weapon scripts that overrode InitSkins to register custom textures and materials for vanilla firearms. Removed files include Paintable Fal, Paintable M4A1, Paintable B95, Paintable CZ527, Paintable Mosin, Paintable Scout, Paintable Winchester, and Paintable SKS under _UFramework/scripts/4_World/UF/Entities/Vanilla Firearms. Cleanup of redundant/obsolete skin registration files (skins likely consolidated or handled elsewhere).

--- _UFramework/scripts/4_World/UF/Entities/Vanilla Firearms/Automatic Rifles/Paintable Fal.c
+++
@@ -1,8 +0,0 @@
-modded class FAL_Base
-{
-	override void InitSkins() {
-		super.InitSkins();
-		RegisterTextureAndMaterial("Vanilla", "DZ\\weapons\\firearms\\fal\\data\\fal_co.paa","DZ\\weapons\\firearms\\fal\\data\\fal.rvmat");
-		RegisterTextureAndMaterial("Camo", "_UFramework\\data\\Firearms\\Fal\\Fal_Camo_Co.paa","DZ\\weapons\\firearms\\fal\\data\\fal.rvmat");
-	}
-};
--- _UFramework/scripts/4_World/UF/Entities/Vanilla Firearms/Bolt Action Rifles/Paintable Scout.c
+++
@@ -1,9 +0,0 @@
-modded class Scout_Base
-{
-	override void InitSkins()
-	{
-		super.InitSkins();
-		RegisterTextureAndMaterial("Green", "dz\\weapons\\firearms\\scout\\data\\scout_grn_co.paa","dz\\weapons\\firearms\\scout\\data\\scout.rvmat");
-		RegisterTextureAndMaterial("Strip", "dz\\weapons\\firearms\\scout\\data\\scout_co.paa","dz\\weapons\\firearms\\scout\\data\\scout.rvmat");
-	}
-};
