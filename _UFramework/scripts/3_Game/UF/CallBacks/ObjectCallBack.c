fd8b956bb64fef4e73e2311be4ed3467e80f06a6
Daemon Forge <kevhoddinott@gmail.com>
2026-03-22 17:39:45 -0400

    Remove debug logging from UNestedCallBack
    Remove numerous UFLog.Debug calls from UNestedCallBack (constructor, OnError, OnTimeout, OnSuccess) to reduce log noise. Functionality and callback forwarding behavior are unchanged; only diagnostic logging lines were deleted.

--- _UFramework/scripts/3_Game/UF/CallBacks/ObjectCallBack.c
+++ _UFramework/scripts/3_Game/UF/CallBacks/ObjectCallBack.c
@@ -344,7 +344,6 @@ class UNestedCallBack : UFRestCallBackBase
 	void UNestedCallBack(UFCallbackBase cb){
 		m_CB = cb;
 		m_UFid = -1;
-		UFLog.Debug("[UNestedCallBack] Created with callback: " + cb.ToString());
 	}
 	
 	void ~UNestedCallBack(){
@@ -357,9 +356,7 @@ class UNestedCallBack : UFRestCallBackBase
 	 * @param errorCode The REST error code
 	 */
 	override void OnError(int errorCode) {
-		UFLog.Debug("[UNestedCallBack] OnError - CID: " + m_UFid + ", ErrorCode: " + errorCode);
 		if (UF().IsCallCanceled(m_UFid)){
-			UFLog.Debug("Call " + m_UFid + " not called as it was requested to be canceled - OnError " + UF().ErrorToString(errorCode));
 			super.OnError(errorCode);
 			return;
 		}
@@ -367,7 +364,6 @@ class UNestedCallBack : UFRestCallBackBase
 		if (errorCode == ERestResultState.EREST_ERROR_CLIENTERROR){
 			rstatus = UF_CLIENTERROR;
 		}
-		UFLog.Debug("[UNestedCallBack] Forwarding OnError to callback, status: " + rstatus);
 		GetCB().OnError(rstatus, m_UFid);
 		super.OnError(errorCode);
 	};
@@ -377,14 +373,10 @@ class UNestedCallBack : UFRestCallBackBase
 	 * Forwards UF_TIMEOUT status to nested callback
 	 */
 	override void OnTimeout() {
-		UFLog.Debug("[UNestedCallBack] OnTimeout - CID: " + m_UFid);
 		if (UF().IsCallCanceled(m_UFid)){
-			UFLog.Debug("Call " + m_UFid + " not called as it was requested to be canceled - OnTimeout");
 			super.OnTimeout();
 			return;
 		}
-		
-		UFLog.Debug("[UNestedCallBack] Forwarding OnTimeout to callback");
 		GetCB().OnError(UF_TIMEOUT, m_UFid);
 		super.OnTimeout();
 	};
@@ -396,19 +388,15 @@ class UNestedCallBack : UFRestCallBackBase
 	 * @param dataSize Size of the response data
 	 */
 	override void OnSuccess(string data, int dataSize) {
-		UFLog.Debug("[UNestedCallBack] OnSuccess - CID: " + m_UFid + ", DataSize: " + dataSize);
 		if (UF().IsCallCanceled(m_UFid)){
-			UFLog.Debug("Call " + m_UFid + " not called as it was requested to be canceled - OnSuccess");
 			super.OnSuccess(data, dataSize);
 			return;
 		}
 		if (dataSize <= 0 || data == "{}" || data == "" || data == "{ }"){
-			UFLog.Debug("[UNestedCallBack] Empty data, forwarding as UF_EMPTY");
 			GetCB().OnError(UF_EMPTY, m_UFid);
 			super.OnSuccess(data, dataSize);
 			return;
 		}
-		UFLog.Debug("[UNestedCallBack] Forwarding OnSuccess to callback, data: " + data.Substring(0, Math.Min(200, data.Length())));
 		GetCB().OnSuccess(data, m_UFid);
 		super.OnSuccess(data, dataSize);
 	};
